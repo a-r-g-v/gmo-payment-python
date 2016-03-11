df8c00e5c972a85468e3e5e294e498742f652266
Yuki Mukasa <mukasa@globalnet-ex.com>
2016-03-11 15:43:21 +0900

    initial commit

---
+++ gmopg/__init__.py
@@ -0,0 +1,30 @@
+import simplejson as json
+import requests
+
+API_BASE_URL = ''
+DEFAULT_TIMEOUT = 30
+
+
+class Error(Exception):
+    pass
+
+
+class BaseAPI(object):
+
+    def __init__(self, timeout=DEFAULT_TIMEOUT):
+        self.timeout = timeout
+
+    def _requests(self, method, path, **kwargs):
+
+        response = method(API_BASE_URL + path, timeout=self.timeout, **kwargs)
+
+        response.raise_for_status()
+
+        # TODO: Check Error
+        return response
+
+    def get(self, path, **kwargs):
+        return self._requests(requests.get, path, **kwargs)
+
+    def post(self, path, **kwargs):
+        return self._requests(requests.post, path, **kwargs)
