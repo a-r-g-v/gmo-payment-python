1d320b1aad06e431b616167bec729e59cae33bb2
a-r-g-v <info@arg.vc>
2017-06-13 05:36:49 +0900

    upgrade 0.0.3

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='gmopg',
-      version='0.0.2',
+      version='0.0.3',
       description='Python API Client for GMO Payment Gateway',
       license="MIT",
       keywords="gmopg",
@@ -9,5 +9,5 @@ setup(name='gmopg',
       author_email='info@arg.vc',
       url='https://github.com/a-r-g-v/gmo-payment-python',
       packages=['gmopg'],
-      install_requires=['requests', 'six', 'typing'])
+      install_requires=['requests', 'six', 'typing']
       )
